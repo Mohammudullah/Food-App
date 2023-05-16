f2ab514d3d47e336b7486fa74193cd7eae565af5
Mohammudullaha Khan <73356760+Mohammudullah@users.noreply.github.com>
2023-05-16 09:15:36 +0600

    Hey I removed inertia

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <title>Food Order App</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
-    @inertiaHead
+    
   </head>
   <body>
     @inertia
